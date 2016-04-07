5042ae38daac090dc3eed5569cb0d869c1a7f7eb
Patrick Dubroy <pdubroy@gmail.com>
2016-04-07 18:16:44 +0200

    Move web stuff into the static/ dir.

--- server.js
+++ server.js
@@ -1,6 +1,40 @@
+/* eslint-env node */
+
+var assert = require('assert');
+var dataurl = require('dataurl');
+var fs = require('fs');
+var textBody = require('body');
+var path = require('path');
+
+// Handlers for requests to `/saveImage`.
+function saveImage(req, res) {
+  textBody(req, function(err, data) {
+    assert(!err);
+    var info = dataurl.parse(data);
+    assert.equal(info.mimetype, 'image/png');
+
+    var filename = path.join(__dirname, 'image.png');
+    fs.writeFile(filename, info.data, {}, function(err) {
+      if (!err) {
+        console.error(err);
+      }
+    });
+    res.end('ok');
+  });
+}
+
 // See http://www.browsersync.io/docs/options/ for more information.
 require('browser-sync')({
   files: ['*'],
-  server: {baseDir: './'},
+  server: {
+    baseDir: 'static',
+    middleware: function(req, res, next) {
+      if (req.url === '/saveImage') {
+        saveImage(req, res);
+      } else {
+        next();
+      }
+    }
+  },
   startPath: '/index.html'
 });
---
+++ static/index.js
@@ -0,0 +1,89 @@
+/* eslint-env browser */
+/* global DocumentTouch */
+
+(function() {
+  var canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext('2d');
+  var canvasChanged = false;
+
+  // Helpers
+  // -------
+
+  function isTouchDevice() {
+    return 'ontouchstart' in window ||
+           !!(window.DocumentTouch && document instanceof DocumentTouch);
+  }
+
+  function handleResize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
+
+  function handleTouchStart(e) {
+    ctx.beginPath();
+    ctx.lineCap = 'round';
+    ctx.lineWidth = 4;
+    ctx.moveTo(e.clientX, e.clientY);
+  }
+
+  function handleTouchMove(e) {
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+    canvasChanged = true;
+  }
+
+  function handleTouchEnd(e) {
+    ctx.closePath();
+  }
+
+  function maybeSaveImage() {
+    if (canvasChanged) {
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        console.log(xhr.status);
+      };
+      xhr.open('POST', 'saveImage', true);
+      xhr.send(canvas.toDataURL());
+    }
+  }
+
+  // Main
+  // ----
+
+  window.addEventListener('resize', handleResize);
+  handleResize();
+
+  if (isTouchDevice()) {
+    canvas.ontouchstart = function(e) {
+      if (e.touches.length === 1) {
+        handleTouchStart(e.touches[0]);
+      }
+    };
+    canvas.ontouchmove = function(e) {
+      if (e.touches.length === 1) {
+        handleTouchMove(e.touches[0]);
+        e.preventDefault();
+      }
+    };
+    canvas.ontouchend = function(e) {
+      handleTouchEnd(e.touches[0]);
+    };
+  } else {
+    var drawing = false;
+    canvas.onmousedown = function(e) {
+      drawing = true;
+      handleTouchStart(e);
+    };
+    canvas.onmousemove = function(e) {
+      if (drawing) {
+        handleTouchMove(e);
+      }
+    };
+    canvas.onmouseup = function(e) {
+      drawing = false;
+      handleTouchEnd(e);
+    };
+  }
+
+  window.setInterval(maybeSaveImage, 1000);
+})();
